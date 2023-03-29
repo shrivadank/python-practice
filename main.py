a5350aaff007de951dab96994c37b4d045c2928a
shrivadan <89403927+shrivadank@users.noreply.github.com>
2023-03-29 10:42:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,15 @@ print("b: " + b)
 
 
 
+#project
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
+
+
+
 ----------------------------------------------------------------------------------
 
 
