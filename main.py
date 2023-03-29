074e9066fc3a7d10352a5a7f8eeb5d39506003f1
shrivadan <89403927+shrivadank@users.noreply.github.com>
2023-03-29 10:27:28 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+DAY-1
+
+#printing
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+
+#Debugging practice
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+#Input Function With Length
+
+print(len(input('what is your name?')))
+
+
+#Python Variables And Swap values
+
+
+a = input("a: ")
+b = input("b: ")
+c= a
+a= b
+b= c
+print("a: " + a)
+print("b: " + b)
+
+
+
+----------------------------------------------------------------------------------
+
+
+
+
